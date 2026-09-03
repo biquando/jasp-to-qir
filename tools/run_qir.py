545efa21afc8339ff5af3c2a3889c1cbd74b753d
Quan Do <biquando@gmail.com>
2026-09-03 00:17:07 -0700

    Add number of shots to run_qir.py

--- tools/run_qir.py
+++ tools/run_qir.py
@@ -11,7 +11,8 @@ from selene_sim import BitcodeString, Quest, build
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument("program", type=Path, help="QIR LLVM IR file")
-parser.add_argument("n_qubits", type=int, help="QuEST simulator capacity")
+parser.add_argument("-q", "--qubits", type=int, required=True, help="QuEST simulator capacity")
+parser.add_argument("-s", "--shots", type=int, help="number of shots")
 args = parser.parse_args()
 
 target = "aarch64" if platform.machine().lower() in {"arm64", "aarch64"} else "x86-64"
@@ -19,8 +20,20 @@ qir = args.program.read_text(encoding="utf-8")
 qis = qir_to_qis(qir_ll_to_bc(qir), target=target)
 runner = build(BitcodeString(qis))
 
-for label, value in runner.run(Quest(), n_qubits=args.n_qubits):
-    if type(value) is list:
-        print(f"{label}: {''.join([str(v) for v in value])}")
-    else:
-        print(f"{label}: {value}")
+if args.shots is not None:
+    results = runner.run_shots(Quest(), n_qubits=args.qubits, n_shots=args.shots)
+    for i, shot in enumerate(results):
+        print(f'===== SHOT {i+1} =====')
+        for label, value in shot:
+            if type(value) is list:
+                print(f"{label}: {''.join([str(v) for v in value])}")
+            else:
+                print(f"{label}: {value}")
+        print()
+else:
+    results = runner.run(Quest(), n_qubits=args.qubits)
+    for label, value in results:
+        if type(value) is list:
+            print(f"{label}: {''.join([str(v) for v in value])}")
+        else:
+            print(f"{label}: {value}")
